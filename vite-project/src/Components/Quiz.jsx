7da9cfb10849647b057e5609bdb1d42ec9dec438
Vansh <86408597+vansh1505@users.noreply.github.com>
2025-01-07 16:07:56 +0530

    Update Quiz.jsx

--- vite-project/src/Components/Quiz.jsx
+++ vite-project/src/Components/Quiz.jsx
@@ -31,42 +31,64 @@ const Quiz = () => {
   };
 
   return (
-    <>
-      <div className='flex justify-center items-center flex-col gap-12 '>
-        <h1 className='text-white text-3xl tracking-wide'></h1>
-        <div className='glass-effect text-white p-5 flex flex-col gap-5 rounded-xl'>
-          <h2 className='text-xl'>
-            {index + 1}. {question.text}
-          </h2>
-          <ul className='flex flex-col gap-5'>
-            <li ref={option1} className={`border p-3 bg-white/30 rounded-lg cursor-pointer ${selectedOption === 1 ? 'bg-white/50' : ''}`} onClick={() => checkans(1)}>
-              {question.options[0].text}
-            </li>
-            <li ref={option2} className={`border p-3 bg-white/30 rounded-lg cursor-pointer ${selectedOption === 2 ? 'bg-white/50' : ''}`} onClick={() => checkans(2)}>
-              {question.options[1].text}
-            </li>
-            <li ref={option3} className={`border p-3 bg-white/30 rounded-lg cursor-pointer ${selectedOption === 3 ? 'bg-white/50' : ''}`} onClick={() => checkans(3)}>
-              {question.options[2].text}
-            </li>
-            <li ref={option4} className={`border p-3 bg-white/30 rounded-lg cursor-pointer ${selectedOption === 4 ? 'bg-white/50' : ''}`} onClick={() => checkans(4)}>
-              {question.options[3].text}
-            </li>
-          </ul>
-          <div className='flex justify-center flex-col items-center gap-5'>
-            <button
-              onClick={next}
-              className={`bg-blue-600 w-fit py-2 px-8 rounded-2xl hover:bg-blue-800 duration-200 ${!lock ? 'opacity-50 cursor-not-allowed' : ''}`}
-              disabled={!lock}
-            >
-              Next
-            </button>
-            <p>
-              {index + 1} of {data.length} Questions
-            </p>
-          </div>
+    <div className='flex justify-center items-center flex-col gap-12'>
+      <h1 className='text-white text-3xl tracking-wide'></h1>
+      <div className='glass-effect text-white p-5 flex flex-col gap-5 rounded-xl'>
+        <h2 className='text-xl'>
+          {index + 1}. {question.text}
+        </h2>
+        <ul className='flex flex-col gap-5'>
+          <li
+            ref={option1}
+            className={`border p-3 bg-white/30 rounded-lg cursor-pointer 
+              ${selectedOption === 1 ? 'bg-white/50' : ''}
+              ${selectedOption !== 1 ? 'hover:bg-white/40' : ''}`}
+            onClick={() => checkans(1)}
+          >
+            {question.options[0].text}
+          </li>
+          <li
+            ref={option2}
+            className={`border p-3 bg-white/30 rounded-lg cursor-pointer 
+              ${selectedOption === 2 ? 'bg-white/50' : ''}
+              ${selectedOption !== 2 ? 'hover:bg-white/40' : ''}`}
+            onClick={() => checkans(2)}
+          >
+            {question.options[1].text}
+          </li>
+          <li
+            ref={option3}
+            className={`border p-3 bg-white/30 rounded-lg cursor-pointer 
+              ${selectedOption === 3 ? 'bg-white/50' : ''}
+              ${selectedOption !== 3 ? 'hover:bg-white/40' : ''}`}
+            onClick={() => checkans(3)}
+          >
+            {question.options[2].text}
+          </li>
+          <li
+            ref={option4}
+            className={`border p-3 bg-white/30 rounded-lg cursor-pointer 
+              ${selectedOption === 4 ? 'bg-white/50' : ''}
+              ${selectedOption !== 4 ? 'hover:bg-white/40' : ''}`}
+            onClick={() => checkans(4)}
+          >
+            {question.options[3].text}
+          </li>
+        </ul>
+        <div className='flex justify-center flex-col items-center gap-5'>
+          <button
+            onClick={next}
+            className={`bg-blue-600 w-fit py-2 px-8 rounded-2xl hover:bg-blue-800 duration-200 ${!lock ? 'opacity-50 cursor-not-allowed' : ''}`}
+            disabled={!lock}
+          >
+            Next
+          </button>
+          <p>
+            {index + 1} of {data.length} Questions
+          </p>
         </div>
       </div>
-    </>
+    </div>
   );
 };
 
