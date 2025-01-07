07070b2f01f7975d291b3dcbe04c0f036f4d20ed
vansh1505 <vanshsharma.vs321@gmail.com>
2025-01-07 14:39:21 +0530

    updated welcome final

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,22 +1,13 @@
+import React from 'react'
 import Quiz from './components/Quiz'
+import Welcome from './Components/Welcome'
 function App() {
 
   return (
     <>
-    <div className="bg-gradient-to-br from-custom-red-1 via-custom-red-4 to-custom-orange-4 h-screen flex justify-center items-center">
-        {showQuiz ? (
-          <Quiz />
-        ) : (
-          <div className="text-center">
-            <h1 className="text-white text-4xl mb-8">Welcome to TechFest Quiz</h1>
-            <button
-              onClick={handleStartQuiz}
-              className="px-6 py-3 bg-black text-white font-semibold rounded-lg shadow-lg hover:bg-orange-500 transition duration-300"
-            >
-              Start Quiz
-            </button>
-          </div>
-        )}
+    <div className="bg-gradient-to-br from-custom-red-1 via-custom-red-4 to-custom-orange-4 h-full justify-center items-center w-full">
+        <Welcome />
+        <Quiz />
       </div>
     </>
   )
--- vite-project/src/Components/Quiz.jsx
+++ vite-project/src/Components/Quiz.jsx
@@ -5,28 +5,12 @@ const Quiz = () => {
   let [index, setIndex] = useState(0);
   let [question, setQuestion] = useState(data[index]);
   let [lock, setLock] = useState(false);
-  let [score, setScore] = useState(0);
-  let [result, setResult] = useState(false);
+  let [selectedOption, setSelectedOption] = useState(null);
 
   let option1 = useRef(null);
   let option2 = useRef(null);
   let option3 = useRef(null);
   let option4 = useRef(null);
-  let option_arr = [option1, option2, option3, option4];
-
-  const checkans = (e, ans) => {
-    if (!lock) {
-      if (question.ans === ans) {
-        e.target.classList.add('bg-green-400', 'bg-opacity-40', 'border-green-600');
-        setScore((prev) => prev + 1);
-        setLock(true);
-      } else {
-        e.target.classList.add('bg-red-400', 'bg-opacity-40', 'border-red-600');
-        option_arr[question.ans - 1].current.classList.add('bg-green-400', 'bg-opacity-40', 'border-green-600');
-        setLock(true);
-      }
-    }
-  };
 
   const next = () => {
     if (lock) {
@@ -37,72 +21,49 @@ const Quiz = () => {
       setIndex(index + 1);
       setQuestion(data[index + 1]);
       setLock(false);
-      option_arr.forEach((option) => {
-        option.current.classList.remove('bg-green-400', 'bg-red-400', 'bg-opacity-40', 'border-red-600', 'border-green-600');
-      });
+      setSelectedOption(null);
     }
   };
 
-  const reset = () => {
-    setIndex(0);
-    setQuestion(data[0]);
-    setScore(0);
-    setLock(false);
-    setResult(false);
-    option_arr.forEach((option) => {
-      option.current.classList.remove('bg-green-400', 'bg-red-400', 'bg-opacity-40', 'border-red-600', 'border-green-600');
-    });
+  const checkans = (option) => {
+    setSelectedOption(option);
+    setLock(true);
   };
 
   return (
     <>
-      <div className='flex justify-center items-center flex-col gap-12 m-4'>
-        <h1 className='text-white text-3xl tracking-wide'>Quiz For Exoplanets</h1>
-        <div className='glass-effect text-white p-5 flex w-1/2 flex-col gap-5 rounded-xl'>
-          {result ? (
-            <>
-              <div className='flex flex-col justify-center items-center gap-8'>
-                <h2 className='text-center'>
-                  Your Score is {score} out of {data.length}
-                </h2>
-                <button onClick={reset} className='bg-blue-600 w-fit py-2 px-8 rounded-2xl hover:bg-blue-800 duration-200'>
-                  Reset
-                </button>
-              </div>
-            </>
-          ) : (
-            <>
-              <h2 className='text-xl'>
-                {index + 1}. {question.question}
-              </h2>
-              <ul className='flex flex-col gap-5'>
-                <li ref={option1} className='border bg-white/30 backdrop-blur-3xl p-4 rounded-lg cursor-pointer' onClick={(e) => checkans(e, 1)}>
-                  {question.option1}
-                </li>
-                <li ref={option2} className='border p-4 rounded-lg cursor-pointer' onClick={(e) => checkans(e, 2)}>
-                  {question.option2}
-                </li>
-                <li ref={option3} className='border p-4 rounded-lg cursor-pointer' onClick={(e) => checkans(e, 3)}>
-                  {question.option3}
-                </li>
-                <li ref={option4} className='border p-4 rounded-lg cursor-pointer' onClick={(e) => checkans(e, 4)}>
-                  {question.option4}
-                </li>
-              </ul>
-              <div className='flex justify-center flex-col items-center gap-5'>
-                <button
-                  onClick={next}
-                  className={`bg-blue-600 w-fit py-2 px-8 rounded-2xl hover:bg-blue-800 duration-200 ${!lock ? 'opacity-50 cursor-not-allowed' : ''}`}
-                  disabled={!lock}
-                >
-                  Next
-                </button>
-                <p>
-                  {index + 1} of {data.length} Questions
-                </p>
-              </div>
-            </>
-          )}
+      <div className='flex justify-center items-center flex-col gap-12 '>
+        <h1 className='text-white text-3xl tracking-wide'></h1>
+        <div className='glass-effect text-white p-5 flex flex-col gap-5 rounded-xl'>
+          <h2 className='text-xl'>
+            {index + 1}. {question.text}
+          </h2>
+          <ul className='flex flex-col gap-5'>
+            <li ref={option1} className={`border bg-white/30 backdrop-blur-3xl p-3 rounded-lg cursor-pointer ${selectedOption === 1 ? 'bg-white/50' : ''}`} onClick={(e) => checkans(e, 1)}>
+              {question.options[0].text}
+            </li>
+            <li ref={option2} className={`border p-3 bg-white/30 rounded-lg cursor-pointer ${selectedOption === 2 ? 'bg-white/50' : ''}`} onClick={() => checkans(2)}>
+              {question.options[1].text}
+            </li>
+            <li ref={option3} className={`border p-3 bg-white/30 rounded-lg cursor-pointer ${selectedOption === 3 ? 'bg-white/50' : ''}`} onClick={() => checkans(3)}>
+              {question.options[2].text}
+            </li>
+            <li ref={option4} className={`border p-3 bg-white/30 rounded-lg cursor-pointer ${selectedOption === 4 ? 'bg-white/50' : ''}`} onClick={() => checkans(4)}>
+              {question.options[3].text}
+            </li>
+          </ul>
+          <div className='flex justify-center flex-col items-center gap-5'>
+            <button
+              onClick={next}
+              className={`bg-blue-600 w-fit py-2 px-8 rounded-2xl hover:bg-blue-800 duration-200 ${!lock ? 'opacity-50 cursor-not-allowed' : ''}`}
+              disabled={!lock}
+            >
+              Next
+            </button>
+            <p>
+              {index + 1} of {data.length} Questions
+            </p>
+          </div>
         </div>
       </div>
     </>
